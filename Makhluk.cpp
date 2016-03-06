0a81e3fa3a23b563e9f4c1eb9cc61b607b237d0f
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2016-03-06 16:24:25 +0700

    Update Makhluk.cpp

--- Makhluk.cpp
+++ Makhluk.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 Makhluk::Makhluk(){
-	/*Constructor Makhluk Abstrak
-	semua atribut diinisialisasi 0
+	/*! \var Makhluk()
+	\brief Constructor Makhluk Abstrak semua atribut diinisialisasi 0
 	*/
 	cout<<"ctor Makhluk"<<endl;
 	age = 0;
@@ -16,8 +16,9 @@ Makhluk::Makhluk(){
 }
 
 Makhluk::Makhluk(const Makhluk& M){
-	/*Copy Constructor Makhluk Abstrak
-	@param M <M merupakan Makhluk abstrak dimana semua atributnya akan di copy ke Makhluk ini>
+	/* \var Makhluk(const Makhluk& M)
+	\brief Copy Constructor Makhluk Abstrak
+	\param M M merupakan Makhluk abstrak dimana semua atributnya akan di copy ke Makhluk ini
 	*/
 	cout<<"cctor Makhluk"<<endl;
 	age = M.age;
@@ -28,7 +29,8 @@ Makhluk::Makhluk(const Makhluk& M){
 }
 
 Makhluk::~Makhluk(){
-	/*Destuctor Makhluk Abstrak
+	/* \var ~Makhluk()
+	\brief Destuctor Makhluk Abstrak
 	*/
 	cout<<"dtor Makhluk"<<endl;
 }
