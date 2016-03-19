2e5374033496ca7f0e2dd809c18c1f645c42818a
Azzahid <azzahidadhitya@yahoo.com>
2016-03-19 11:56:47 +0700

    Added files via upload
    Added files via upload
    
    Added files via upload
    
    Added files via upload
    
    Added files via upload

--- Makhluk.cpp
+++ Makhluk.cpp
@@ -1,75 +1,57 @@
+/*! 
+ * Implementasi Makhluk
+ * Pengembang : BLP (13514047) - JS (13514001) */
+
 #include "Makhluk.h"
 #include <iostream>
 
 using namespace std;
 
-Makhluk::Makhluk(){
-	/*! \var Makhluk()
-		\brief Constructor Makhluk Abstrak semua atribut diinisialisasi 0
-	*/
-	cout<<"ctor Makhluk"<<endl;
-	age = 0;
-	powerlevel = 0;
-	deltaT = 0;
-	x = 0;
-	y = 0;
-}
-
-Makhluk::Makhluk(const Makhluk& M){
-	/* ! \var Makhluk(const Makhluk& M)
-		\brief Copy Constructor Makhluk Abstrak
-		\param M M merupakan Makhluk abstrak dimana semua atributnya akan di copy ke Makhluk ini
-	*/
-	cout<<"cctor Makhluk"<<endl;
-	age = M.age;
-	powerlevel = M.powerlevel;
-	deltaT = M.deltaT;
-	x = M.x;
-	y = M.y;
-}
-
-Makhluk::~Makhluk(){
-	/* ! \var ~Makhluk()
-	\brief Destuctor Makhluk Abstrak
-	*/
-	cout<<"dtor Makhluk"<<endl;
-}
-
-Makhluk& Makhluk::operator= (const Makhluk& M){
-	/* ! \var Makhluk& operator= (Makhluk& M)
-	\brief Operator = Makhluk Abstrak
-	\param M M merupakan Makhluk abstrak dimana semua atributnya akan di copy ke Makhluk ini
-	*/
-	cout<<"operator = Makhluk"<<endl;
-	age = M.age;
-	powerlevel = M.powerlevel;
-	deltaT = M.deltaT;
-	x = M.x;
-	y = M.y;
-	return *this;
-}
-
 int Makhluk::getpowerlevel(){
-	/* ! \var int getpowerlevel()
+	/*! \var int getpowerlevel()
 	\brief Memberikan keluaran isi dari powerlevel
 	*/
 	return powerlevel;
 }
 
 int Makhluk::getage() {
-	/* ! \var int getage()
+	/*! \var int getage()
 	\brief Memberikan keluaran isi dari age
 	*/
 	return age;	
 }
 
 double Makhluk::getdeltaT() {
-	/* ! \var double getdeltaT()
+	/*! \var double getdeltaT()
 	\brief Memberikan keluaran isi dari deltaT
 	*/
 	return deltaT;
 }
 
-void Makhluk::showme() {
+void Makhluk::getme() {
+	/*! \var void getme()
+	\brief Menampilkan makhluk ini ke layar
+	*/
 	cout << bentuk;	
+}
+
+int Makhluk::getX() {
+	/*! \var int getX()
+	\brief Memberikan keluaran isi dari X - posisi X
+	*/
+	return x;
+}
+
+int Makhluk::getY() {
+	/*! \var int getY()
+	\brief Memberikan keluaran isi dari Y - posisi Y
+	*/
+	return y;
+}
+
+char Makhluk::whome() {
+	/*! \var char whome()
+	\brief Memberikan keluaran isi dari bentuk
+	*/
+	return bentuk;
 }
--- Makhluk.h
+++ Makhluk.h
@@ -1,23 +1,28 @@
+/*! 
+ * Definisi Kelas Abstrak Makhluk
+ * Pengembang : BLP (13514047) - JS (13514001) */
+
 #ifndef MAKHLUK_H
 #define MAKHLUK_H
-/* Definisi Kelas Abstrak Makhluk */
-
 
 class Makhluk {
 	public :
-		Makhluk();
-		Makhluk(const Makhluk&);
-		~Makhluk();
-		Makhluk& operator= (const Makhluk&);
+		//method
 		virtual void spawn(int a, int b)=0 ;
 		virtual void move() =0;
 		virtual void dead() =0;
 		int getpowerlevel();
 		int getage();
 		double getdeltaT();
-		void showme();
+		char whome();
+		void getme();
+		
+
+		int getX();
+		int getY();
 		
 	protected :
+		//karakter makhluk secara umum
 		char bentuk;
 		int age;
 		int powerlevel;
