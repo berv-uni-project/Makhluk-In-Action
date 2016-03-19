d4d507ecb580c36937ea6e8ba49c5f8053a00ccf
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2016-03-19 11:04:16 +0700

    Update HimpMakhluk.cpp

--- HimpMakhluk.cpp
+++ HimpMakhluk.cpp
@@ -1,5 +1,7 @@
 #include "HimpMakhluk.h"
 
+using namespace std;
+
 HimpMakhluk::HimpMakhluk() {
   
 }
@@ -8,8 +10,8 @@ HimpMakhluk::~HimpMakhluk() {
   
 }
 
-void HimpMakhluk::AddMakhluk() {
-  
+void HimpMakhluk::AddMakhluk(Makhluk *A) {
+  Mem.push_back(A);
 }
 
 void HimpMakhluk::SurvFight() {
@@ -17,13 +19,31 @@ void HimpMakhluk::SurvFight() {
 }
 
 Makhluk * HimpMakhluk::DeleteMakhluk(Makhluk * A) {
-  
+  Makhluk* DelMakhluk = NULL;
+  if (!Mem.empty())
+  {
+    bool found = false;
+    vector<Makhluk *>::iterator it = Mem.begin();
+    do {
+      if (*it->bentuk == A->bentuk) //cek identitas
+      {
+        DelMakhluk = *it;
+        Mem.erase(it);
+        found = true;
+      } else
+      {
+        it++;
+      }
+      
+    } while ((it!=Mem.end()) && (!found));
+  }
+  return DelMakhluk;
 }
 
 bool HimpMakhluk::IsFull() {
-  
-}
-
-bool HimpMakhluk::IsEmpty() {
-
+  if (Mem.size()==MaxMakhluk) {
+    return true;
+  } else {
+    return false;
+  }
 }
