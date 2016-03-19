458d5fdbce493ddd752e3d33f8bb5fe89b842e50
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2016-03-19 10:42:28 +0700

    Update HimpMakhluk.cpp

--- HimpMakhluk.cpp
+++ HimpMakhluk.cpp
@@ -1,1 +1,29 @@
+#include "HimpMakhluk.h"
 
+HimpMakhluk::HimpMakhluk() {
+  
+}
+
+HimpMakhluk::~HimpMakhluk() {
+  
+}
+
+void HimpMakhluk::AddMakhluk() {
+  
+}
+
+void HimpMakhluk::SurvFight() {
+  
+}
+
+Makhluk * HimpMakhluk::DeleteMakhluk(Makhluk * A) {
+  
+}
+
+bool HimpMakhluk::IsFull() {
+  
+}
+
+bool HimpMakhluk::IsEmpty() {
+
+}
